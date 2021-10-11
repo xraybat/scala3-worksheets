f52917ccea7dc455ddd0da5859bd8e6095ec1401
xraybat <xraybat100@gmail.com>
2021-10-11 17:18:00 +1100

    extra

--- src/main/scala/UnionsErrors.scala
+++ src/main/scala/UnionsErrors.scala
@@ -10,42 +10,76 @@ enum Error2:
   case Code4, Code5, Code6
 end Error2
 
+def service0: Either[Error1, String] = Right("it's fine!")
 def service1: Either[Error1, String] = Left(Error1.Code3)
 def service2: Either[Error2, String] = Left(Error2.Code4)
+
+def program0: Either[Error1, String] = 
+  for
+    result0 <- service0
+  yield 
+    s"program0: $result0"
+end program0
 
 def program1: Either[Error1, String] = 
   for
     result1 <- service1
   yield 
-    s"hello1, $result1"
+    s"program1: $result1"
 end program1
 
-def program2: Either[Error1 | Error2, String] = 
+def program1and2: Either[Error1 | Error2, String] = 
   for
     result1 <- service1
     result2 <- service2
-  yield 
-    s"hello2, $result1, $result2"
-end program2
+  yield
+    s"program1and2: result1, $result1; result2, $result2"
 
-def combine = 
+end program1and2
+
+def combine1and2 =  // `Either[Error1 | Error2, String]` will be inferred
   for
     prog1 <- program1
-    prog2 <- program2
+    prog1and2 <- program1and2
   yield
-    s"combine, $prog1, $prog2"
-end combine
+    s"combine1and2: program1, $prog1; program1and2, $prog1and2"
+end combine1and2
+
+def combineAll =    // `Either[Error1 | Error2, String]` will be inferred
+  for
+    prog0 <- program0
+    prog1 <- program1
+    prog1and2 <- program1and2
+  yield
+    s"combineAll: program0, $prog0; program1, $prog1; program1and2, $prog1and2"
+end combineAll
 
 @main def unionsErrors: Unit = 
-  program2 match                  // QU??: warning here??
-    case Right(value: String) => ()
-    case Left(err: Error1) => ()
-    case Left(err: Error2) => ()  // commenting this out would lead to compile error 'match may not be exhaustive'
+  println("==="
+    + s"\nprogram0: $program0"
+    + s"\nprogram1: $program1"
+    + s"\nprogram1and2: $program1and2"
+    + s"\ncombineAll: $combineAll"
+    + "\n===\n")
 
-  combine match // note the union gets inferred to `Either[Error1 | Error2, String]` (from program2), not just `Either[Error1, String` from program1
-    case Right(value: String) => ()
-    case Left(err: Error1) => ()
-    case Left(err: Error2) => ()  // commenting this out would lead to compile error 'match may not be exhaustive'
+  program1and2 match  // QU??: warning here??
+    case Right(value: String) => println(value)
+    case Left(err: Error1) => println(err)
+    case Left(err: Error2) => println(err)  // commenting this out would lead to compile error 'match may not be exhaustive'
+
+  combine1and2 match  // note the union gets inferred to `Either[Error1 | Error2, String]` (from program1and2), not just `Either[Error1, String` from program1
+    case Right(value: String) => println(value)
+    case Left(err: Error1) => println(err)
+    case Left(err: Error2) => println(err)  // commenting this out would lead to compile error 'match may not be exhaustive'
+
+  program0 match
+    case Right(value: String) => println(value)
+    case Left(err: Error1) => println(err)
+  
+  combineAll match
+    case Right(value: String) => println(value)
+    case Left(err: Error1) => println(err)
+    case Left(err: Error2) => println(err)  // commenting this out would lead to compile error 'match may not be exhaustive'
 
   println("scala3 got here!")
 
